355d073d4fd0825309d35595ba8a515748438714
rwin336 <rwin336@gmail.com>
2019-03-09 13:34:36 -0500

    Move restapi common code to proper location

---
+++ testhub_restapi/testhub_restapi/common/constants.py
@@ -0,0 +1,15 @@
+
+
+TEST_INFO_SUCCESS = "TestInfoSuccess"
+TEST_INFO_RUNNING = "TestInfoRunning"
+TEST_INFO_FAILED = "TestInfoFailed"
+TEST_INFO_COMPETE = "TestInfoComplete"
+TEST_INFO_PENDING = "TestInfoPending"
+
+FAULT_REQUEST_SUCCESS = "FaultRequestSuccess"
+FAULT_REQUEST_RUNNING = "FaultRequestRunning"
+FAULT_REQUEST_FAILED = "FaultRequestFailed"
+FAULT_REQUEST_COMPLETE = "FaultRequestComplete"
+FAULT_REQUEST_PENDING = "FaultRequestPending"
+
+
---
+++ testhub_restapi/testhub_restapi/common/utils.py
@@ -0,0 +1,65 @@
+import crypt
+import json
+import logging
+import os
+import pecan
+import re
+import yaml
+
+import constants
+import exception
+
+logginr = logging.getLogger(__name__)
+
+
+
+def _get_err(error_type, op_name, err_msg=""):
+    """
+    This function is for Raising Exception for REST API.
+    It has 2 error_type:
+       - raise: Used when REST API is exposed as wsme_pecan.wsexpose
+       - json: Used when REST API is exposed as pecan.expose('json')
+    """
+    if not err_msg:
+        err_msg = "{} Operation is already in progress. Please try " \
+                  "after sometime.".format(op_name)
+
+    logging.error(err_msg)
+    if error_type == "raise":
+        restapi_exception = exception.RestApiException()
+        restapi_exception.message = err_msg
+        restapi_exception.code = 409
+        raise restapi_exception
+    else:
+        error_json = {"debuginfo": "", "faultcode": "Client",
+                      "faultstring": err_msg}
+        return pecan.Response(json.dumps(error_json), 409,
+                              content_type='application/json')
+
+r_type, op_name, err_msg=""):
+    """
+    This function is for Raising Exception for REST API.
+    It has 2 error_type:
+       - raise: Used when REST API is exposed as wsme_pecan.wsexpose
+       - json: Used when REST API is exposed as pecan.expose('json')
+    """
+    if not err_msg:
+        err_msg = "{} Operation is already in progress. Please try " \
+                  "after sometime.".format(op_name)
+
+    logging.error(err_msg)
+    if error_type == "raise":
+        restapi_exception = exception.RestApiException()
+        restapi_exception.message = err_msg
+        restapi_exception.code = 409
+        raise restapi_exception
+    else:
+        error_json = {"debuginfo": "", "faultcode": "Client",
+                      "faultstring": err_msg}
+        return pecan.Response(json.dumps(error_json), 409,
+                              content_type='application/json')
+
+
+
+
+
